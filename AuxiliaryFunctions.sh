abde9b7637e42d2c0c1221542fa048d95408297e
Alessandro Sciarra <sciarra@th.physik.uni-frankfurt.de>
2016-12-13 11:53:36 +0100

    Added first draft of main file and auxiliary functions file

---
+++ AuxiliaryFunctions.sh
@@ -0,0 +1,38 @@
+function ParseCommandLineParameters(){
+    while [ $1 != '' ]; do
+        case $1 in
+            -h | --help )
+                printf "\e[38;5;2m\n"
+                printf "Helper to be written!"
+                printf "\e[0m\n\n"
+                exit 0
+                shift ;;
+            *)
+                printf "\e[38;5;9m\n Unrecognized option \e[1m$1\e[21m! Aborting...\n\n\e[0m"; exit -1; shift ;;
+        esac
+    done
+}
+
+function CreateTexLocaldefsTemplate(){
+    local OUTPUT_FILENAME="$1"
+    #Redirect standard output to file
+    exec 3>&1 1>$OUTPUT_FILENAME
+    #Template production, overwriting the file
+    rm -f $OUTPUT_FILENAME
+    echo '\def\lecture{}   % '
+    echo '\def\professor{} % '
+    echo '\def\tutor{}     % '
+    echo '\def\tutorMail{} % '
+    echo '\def\semester{}  % '
+    #Restore standard output
+    exec 1>&3
+}
+
+function CheckTexLocaldefsTemplate(){
+    local TEMPLATE_FILENAME="$1"
+    #Parse file line by line
+    while read -r LINE || [[ -n "$LINE" ]]; do # [[ -n "$LINE" ]] is to read also last line if it does not end with \n
+        #Add parsing
+    done < "$TEMPLATE_FILENAME"  && unset -v 'LINE'
+
+}
---
+++ ExerciseHandler.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+####################
+# Exercise handler #
+####################
+
+#Sourcing auxiliary files
+source AuxiliaryFunctions.sh || exit -2
+
+#Needed variables
+
+#Parse command line parameters
+ParseCommandLineParameters $@
+
+#Check if template for latex is present, if not or not complete, terminate and warn user
+
+
+
+
+
+#TODO: 1) Put .tex files in TeX folder modifying the template
+#         in order to input Packages.tex, Preamble.tex, Document.tex
+#      2) Create functions to build each of the .tex above
+#      3) Ingredients needed:
+#          - Parser for Exercise file (extraction of user packages, commands)
+#          - Logic with user variables for Preamble commands (e.g. lecture, prof, etc.)
+#      4) Implement interactive part to choose exercise (suppose Pool folder
+#         to be given by the user in a local variable)
+#      5) Implement main part where the exercise are processed, the Sheet.tex
+#         file is created, compiled and deleted (if everything fine, only .pdf should
+#         be kept; maybe do compilation in separate folder so that the user can
+#         investigate in case!?)
+#
+#NOTE: Ideally this git should be only a collection of tools. The user should
+#      elsewhere create one or two localdefs files and then call this file from there.
+#      Therefore, this script should look for the file with the localdefs from where it
+#      is run (give default names and maybe make an option to change their name). 
+#      If this file it is not found, a template should be created!
