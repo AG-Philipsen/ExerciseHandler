2330fa1b0fcad93a91a7be357e6656dba4232feb
Alessandro Sciarra <sciarra@th.physik.uni-frankfurt.de>
2016-12-22 18:37:25 +0100

    Put ExerciseHandler in a first usable beta version
    Worked over everything and added first basic classic theme.
    Clearly it has to be improved, since it is quite ugly.
    Many todos have been left in the main file. Next step is
    to go through them and see where and how to go on working.

--- AuxiliaryFunctions.sh
+++ AuxiliaryFunctions.sh
@@ -1,3 +1,11 @@
+function PrintWarning(){
+    printf "\n\e[38;5;11m \e[1m\e[4mWARNING\e[24m:\e[21m %s\e[0m\n\n" "$1"
+}
+
+function PrintError(){
+    printf "\n\e[38;5;9m \e[1m\e[4mERROR\e[24m:\e[21m %s\e[0m\n\n" "$1"
+}
+
 function ParseCommandLineParameters(){
     while [ "$1" != '' ]; do
         case $1 in
@@ -7,48 +15,183 @@ function ParseCommandLineParameters(){
                 printf "\e[0m\n\n"
                 exit 0
                 shift ;;
+            -n | --newExercise )
+                PRODUCE_NEW_EXERCISE='TRUE'
+                shift ;;
+            -f | --final )
+                printf "\e[38;5;9m\n Option \e[1m$1\e[21m! still to be implemented! Aborting...\n\n\e[0m"; exit -1; shift ;;
             *)
                 printf "\e[38;5;9m\n Unrecognized option \e[1m$1\e[21m! Aborting...\n\n\e[0m"; exit -1; shift ;;
         esac
     done
 }
 
-function CreateTexLocaldefsTemplate(){
-    local OUTPUT_FILENAME="$1"
+function ProduceNewEmptyExercise(){
+    printf "\e[38;5;207m\n Please, insert the exercise filename: \e[0m\e[s"
+    local NEW_EXERCISE_FILENAME
+    while read NEW_EXERCISE_FILENAME; do
+        [ "$NEW_EXERCISE_FILENAME" = '' ] && printf "\e[u\e[1A" && continue
+        if [ -f $EXERCISE_POOL_FOLDER/$NEW_EXERCISE_FILENAME ]; then
+            printf "\n\e[1;38;5;202m File \"$NEW_EXERCISE_FILENAME\" already existing!\e[21m\e[38;5;11m Please, provide a different name: \e[0m\e[s"
+            continue
+        else
+            break
+        fi
+    done
     #Redirect standard output to file
-    exec 3>&1 1>$OUTPUT_FILENAME
+    exec 3>&1 1>$EXERCISE_POOL_FOLDER/$NEW_EXERCISE_FILENAME
+    echo -e '%__BEGIN_PACKAGES__%\n\n%__END_PACKAGES__%\n\n\n'
+    echo -e '%__BEGIN_DEFINITIONS__%\n\n%__END_DEFINITIONS__%\n\n\n'
+    echo -e '%__BEGIN_BODY__%\n\n%__END_BODY__%\n\n\n'
+    #Restore standard output
+    exec 1>&3    
+}
+
+function CreateTexLocaldefsTemplate(){
     #Template production, overwriting the file
-    rm -f $OUTPUT_FILENAME
+    rm -f $TEX_LOCALDEFS_FILENAME
+    #Redirect standard output to file
+    exec 3>&1 1>$TEX_LOCALDEFS_FILENAME
+    echo -e '%__BEGIN_PACKAGES__%\n\n%__END_PACKAGES__%\n\n\n'
+    echo '%__BEGIN_DEFINITIONS__%'
     echo '\def\lecture{}   % '
     echo '\def\professor{} % '
     echo '\def\tutor{}     % '
     echo '\def\tutorMail{} % '
     echo '\def\semester{}  % '
+    echo -e '%__END_DEFINITIONS__%\n\n\n'
     #Restore standard output
     exec 1>&3
 }
 
+function CheckBlocksInFile(){
+    local FILENAME="$1"; shift
+    local BLOCKS_NAME=( $@ )
+    for BLOCK in "${BLOCKS_NAME[@]}"; do
+        if [ $(grep -c "^[[:blank:]]*%__BEGIN_${BLOCK}__%[[:blank:]]*$" $FILENAME) -ne 1 ] || [ $(grep -c "^[[:blank:]]*%__END_${BLOCK}__%[[:blank:]]*$" $FILENAME) -ne 1 ]; then
+            PrintError "Block %__BEGIN_${BLOCK}__%  ->  %__END_${BLOCK}__% not correctly found in \"$FILENAME\" file! Aborting..."; exit -2
+        fi
+    done
+}
+
 function CheckTexLocaldefsTemplate(){
-    local TEMPLATE_FILENAME="$1"
+    local LINE BLOCK
     #Parse file line by line
     while read -r LINE || [[ -n "$LINE" ]]; do # [[ -n "$LINE" ]] is to read also last line if it does not end with \n
-        echo
-        #Add parsing
-    done < "$TEMPLATE_FILENAME"  && unset -v 'LINE'
+        if [ "$(grep -o "{.*}" <<< "$LINE")" = '{}' ]; then
+            PrintWarning "Found empty fields in \"$TEMPLATE_FILENAME\"! Final result could be affected!\e[0m\n\n"
+            return
+        fi
+    done < "$TEX_LOCALDEFS_FILENAME"
+    #General checks on blocks
+    CheckBlocksInFile "$TEX_LOCALDEFS_FILENAME" "PACKAGES" "DEFINITIONS" "BODY"
+} 
 
+function LookForExercisesAndMakeList(){
+    if [ ! -d $EXERCISE_POOL_FOLDER ]; then
+        PrintError "No exercise pool folder \"$EXERCISE_POOL_FOLDER\" has been found! Aborting..."; exit -2
+    fi
+    EXERCISE_LIST=( $(ls $EXERCISE_POOL_FOLDER/*.tex 2> /dev/null | xargs -d '\n' -n 1 basename) )
+    if [ ${#EXERCISE_LIST[@]} -eq 0 ]; then
+        PrintError "No exercise .tex file has been found in pool folder \"$EXERCISE_POOL_FOLDER\"! Aborting..."; exit -2
+    fi
 }
+
+function PrintListOfExercises(){
+    printf "\e[1;38;5;207m\n List of exercises found in the pool\e[21m:\n\n\e[0m"
+    local GIVEN_LIST=( $@ )
+    local INDEX=0
+    for INDEX in "${!GIVEN_LIST[@]}" ; do  GIVEN_LIST[$INDEX]="$(printf "%3d" $((INDEX+1)))) ${GIVEN_LIST[$INDEX]}"; done
+    local COLUMNS_TERMINAL=$(tput cols)
+    local LONGEST_FILENAME_LENGTH=$(printf "%s\n" "${GIVEN_LIST[@]}" | awk '{print length}' | sort -n | tail -n1)
+    local COLUMNS_WIDTH=$((LONGEST_FILENAME_LENGTH+10))
+    local MAX_NUMBER_OF_COLUMNS=$((COLUMNS_TERMINAL/COLUMNS_WIDTH))
+    local FORMAT_STRING=""; for((INDEX=0; INDEX<MAX_NUMBER_OF_COLUMNS; INDEX++)); do FORMAT_STRING+="%-${COLUMNS_WIDTH}s"; done
+    printf "$FORMAT_STRING\n" "${GIVEN_LIST[@]}"
+
+    #TODO: Print list going vertically and not horizontally!
+}
+
+function PickupExercises(){
+    printf "\e[38;5;207m\n Please, insert the exercise numbers that you wish to include in the exercise sheet (sperated by space): \e[0m\e[s"
+    local SELECTED_EXERCISES INDEX
+    local GIVEN_LIST=( $@ )
+    while read -a SELECTED_EXERCISES; do
+        [ ${#SELECTED_EXERCISES[@]} -eq 0 ] && printf "\e[u\e[1A" && continue
+        for INDEX in ${SELECTED_EXERCISES[@]}; do
+            if [[ $INDEX =~ ^[1-9][0-9]*$ ]] && [ $INDEX -le ${#GIVEN_LIST[@]} ]; then
+                continue
+            else
+                printf "\n\e[1;38;5;208m Invalid input!\e[21m\e[38;5;207m Please, insert the \e[1mexercise numbers sperated by space\e[21m: \e[0m\e[s"
+                continue 2
+            fi
+        done
+        break
+    done
+    for INDEX in ${SELECTED_EXERCISES[@]}; do
+        CHOOSEN_EXERCISES+=( ${GIVEN_LIST[$((INDEX-1))]} )
+    done
+}
+
+function CheckChoosenExercises(){
+    for EXERCISE in ${CHOOSEN_EXERCISES[@]}; do
+        CheckBlocksInFile $EXERCISE_POOL_FOLDER/$EXERCISE  "PACKAGES" "DEFINITIONS" "BODY"
+    done    
+}
+
+
+#=========================================================================================================================================================#
+
+function ExtractBlockFromFileAndAppendToAnotherFile(){
+    local INPUT_FILE="$1"
+    local OUTPUT_FILE="$2"
+    local PART_OF_DOCUMENT="$3"
+    awk '/%__BEGIN_'$PART_OF_DOCUMENT'__%/,/%__END_'$PART_OF_DOCUMENT'__%/' $INPUT_FILE | head -n -1 | tail -n +2 >> $OUTPUT_FILE
+}
+
+function ProduceTexAuxiliaryFile(){
+    local OUTPUT_FILE="$1"
+    local PART_OF_DOCUMENT="$2"
+    local EXERCISE
+    ExtractBlockFromFileAndAppendToAnotherFile  $TEX_LOCALDEFS_FILENAME  $OUTPUT_FILE  $PART_OF_DOCUMENT
+    for EXERCISE in ${CHOOSEN_EXERCISES[@]}; do
+        ExtractBlockFromFileAndAppendToAnotherFile  $EXERCISE_POOL_FOLDER/$EXERCISE  $OUTPUT_FILE  $PART_OF_DOCUMENT
+    done
+    #NOTE: We decided to use guards to divide the parts of the exercises file, because the parsing is then easier.
+    #      For example, to extract packages one could do something like,
+    #         awk '{split($0, res, "%"); if(res[1] !~ /^[ ]*$/){print res[1]}}' file.tex | sed 's/^[[:blank:]]*//g' | tr '\n' ' ' | grep -Eo '\\usepackage(\[[^]]*\])?{[^}]+}'
+    #      but there are many cases that could break this down.
+}
+
+function CheckTexPackagesFile(){
+    PrintWarning "Function \"$FUNCNAME\" not implemented yet! It is up to you to avoid conflicts in loading packages!!"
+}
+
+function CheckTexDefinitionsFile(){
+    PrintWarning "Function \"$FUNCNAME\" not implemented yet! It is up to you to avoid conflicts in loading packages!!"
+}
+
 
 function ProduceTexMainFile(){
     local TEX_MAIN_FILENAME="$1"
+    rm -f $TEX_MAIN_FILENAME
     #Redirect standard output to file
     exec 3>&1 1>$TEX_MAIN_FILENAME
     #Template production, overwriting the file
-    rm -f $TEX_MAIN_FILENAME
     echo '\documentclass[a4paper]{article}'
+    echo ''
     echo '\input{Packages}'
-    echo '\input{Preamble}'
-    echo '\input{Localdefs}'
-    echo '\input{Document}'
+    echo ''
+    echo '\input{Definitions}'
+    echo ''
+    echo "\input{$REPOSITORY_DIRECTORY/ClassicTheme}"
+    echo ''
+    echo '\begin{document}'
+    echo '  \Heading'
+    echo '  \Sheet[1]'
+    echo '  %Exercises'
+    echo '  \input{Document}'
+    echo '\end{document}'
     #Restore standard output
     exec 1>&3
 }
--- ExerciseHandler.sh
+++ ExerciseHandler.sh
@@ -13,21 +13,74 @@
 #                                                                                        #
 #----------------------------------------------------------------------------------------#
 
+#Variables
+REPOSITORY_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+INVOKING_DIRECTORY="$(pwd)"
+TEX_LOCALDEFS_FILENAME="TexLocaldefs.tex"
+EXERCISE_POOL_FOLDER="Exercises"
+EXERCISE_LIST=(); CHOOSEN_EXERCISES=() #These arrays contain the basenames of the files
+COMPILATION_FOLDER="TemporaryCompilationFolder"
+EXERCISE_SHEET_NAME="ExerciseSheet"
+PACKAGES_TEX_FILE="Packages.tex"
+#USER_PREAMBLE_TEX_FILE="Preamble.tex"
+DEFINITIONS_TEX_FILE="Definitions.tex"
+DOCUMENT_TEX_FILE="Document.tex"
+PDF_FOLDER="Pdf"
+PRODUCE_NEW_EXERCISE='FALSE'
+
+#Sourcing auxiliary file(s)
+source $REPOSITORY_DIRECTORY/AuxiliaryFunctions.sh || exit -2
+
 #Warning that the script is in developement phase!
-printf "\n\e[38;5;11m \e[1m\e[4mWARNING\e[24m:\e[21m Script under developement! Do not use it!\e[0m\n\n"
-
-#Sourcing auxiliary files
-source AuxiliaryFunctions.sh || exit -2
-
-#Needed variables
+PrintWarning "Script under developement and in a beta phase! Not everything is guaranteed to work!!"
 
 #Parse command line parameters
 ParseCommandLineParameters $@
 
+#If user needs exercise template just produce it and exit
+[ $PRODUCE_NEW_EXERCISE = 'TRUE' ] && ProduceNewEmptyExercise && exit 0
+
 #Check if template for latex is present, if not or not complete, terminate and warn user
+if [ ! -f $TEX_LOCALDEFS_FILENAME ]; then
+    CreateTexLocaldefsTemplate
+    printf "\n\e[38;5;39m \e[1m\e[4mNOTE\e[24m:\e[21m The local definitions file \"$TEX_LOCALDEFS_FILENAME\""
+    printf "has not been found and an empty template to be filled out has been created.\n"
+    printf "       Please, provide required information in it and run again this script.\e[0m\n\n"
+else
+    CheckTexLocaldefsTemplate
+fi
 
+#Present list of exercises and ask user which ones she/he wants
+LookForExercisesAndMakeList
+PrintListOfExercises ${EXERCISE_LIST[@]}
+PickupExercises ${EXERCISE_LIST[@]}
+CheckChoosenExercises
 
+#TeX part: set up main and sub-files before compilation
+[ -d $COMPILATION_FOLDER ] && mv $COMPILATION_FOLDER ${COMPILATION_FOLDER}_$(date +%d.%m.%Y_%H%M%S)
+mkdir $COMPILATION_FOLDER || { PrintError "Cannot create \"$COMPILATION_FOLDER\"! Aborting..." && exit -2; }
+ProduceTexAuxiliaryFile $COMPILATION_FOLDER/$PACKAGES_TEX_FILE    "PACKAGES"
+ProduceTexAuxiliaryFile $COMPILATION_FOLDER/$DEFINITIONS_TEX_FILE "DEFINITIONS"
+ProduceTexAuxiliaryFile $COMPILATION_FOLDER/$DOCUMENT_TEX_FILE    "BODY"
+CheckTexPackagesFile
+CheckTexDefinitionsFile
+ProduceTexMainFile      $COMPILATION_FOLDER/${EXERCISE_SHEET_NAME}.tex
 
+#Compilation
+cd $COMPILATION_FOLDER
+pdflatex -interaction=batchmode ${EXERCISE_SHEET_NAME}.tex >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    PrintError "Error occurred in pdflatex compilation!! No file will be removed from \"$COMPILATION_FOLDER\" directory!"
+else
+    cd $INVOKING_DIRECTORY
+    mkdir -p $PDF_FOLDER
+    NEW_PDF_FILENAME=${EXERCISE_SHEET_NAME}_$(date +%d.%m.%Y_%H%M%S)
+    cp $COMPILATION_FOLDER/${EXERCISE_SHEET_NAME}.pdf $PDF_FOLDER/$NEW_PDF_FILENAME
+    xdg-open $PDF_FOLDER/$NEW_PDF_FILENAME >/dev/null 2>&1 &
+    rm -r $COMPILATION_FOLDER
+fi
+
+exit 0
 
 #NOTE: Ideally this git should be only a collection of tools. The user should
 #      elsewhere create one or two localdefs files and then call this file from there.
@@ -35,22 +88,39 @@ ParseCommandLineParameters $@
 #      is run (give default names and maybe make an option to change their name). 
 #      If this file it is not found, a template should be created and the user warned!
 #      Flow of the script:
-#      1) Check template
-#      2) Ask user for exercises
-#      3) Create main file that basically will input:
-#           Packages.tex  Preamble.tex  Localdefs.tex  Document.tex
+#      1) Check template                                                DONE
+#      2) Ask user for exercises                                        DONE
+#      3) Create main file that basically will input:                   DONE
+#           Packages.tex  Preamble.tex  Localdefs.tex  Document.tex     DONE
 #      4) Create each of the files needed
-#      5) Compile main file (in separate folder!?)
-#      6) Delete aux files if everything is fine (and open pdf!?).
+#           Packages.tex                                                DONE
+#           Preamble.tex                                                DONE
+#           Localdefs.tex                                               DONE
+#           Document.tex                                                DONE
+#      5) Compile main file (in separate folder)                        DONE
+#      6) Delete aux files if everything is fine (and open pdf!?).      DONE
 #
 #TODO: 1) Think about possible bunch of packages always used. Make them up to the user
 #         in a possible file that the user can create locally!?
 #         Ingredients needed:
 #          - Parser for Exercise file (extraction of user packages, commands)
 #          - Logic with user variables for Preamble commands (e.g. lecture, prof, etc.)
-#      2) Implement interactive part to choose exercise (suppose Pool folder
-#         to be given by the user in a local variable)
-#      3) Implement main part where the exercise are processed, the Sheet.tex
+#      2) Implement main part where the exercise are processed, the Sheet.tex
 #         file is created, compiled and deleted (if everything fine, only .pdf should
 #         be kept; maybe do compilation in separate folder so that the user can
 #         investigate in case!?)
+#      3) Implement a --final option in order to move the produced file (also the .tex
+#         in this case) to a separate folder which should be created the first time and
+#         which should have a name related to the lecture (e.g. lectureName_semester).
+#         Think of whether to use this for numbering of sheet.
+#      4) Create a log file mechanism in pool of exercises so that the visualization of
+#         the list of exercises can distinguish between already used exercises (use
+#         colours? Do not show already used exercises?)
+#      5) Trap CTRL-C in a nice way: cleaning files/folders. etc. -> DISCUSS
+#      6) Implement an option to give exercise numbers and therefore skip interactive step!
+#      7) DISCUSS about latex commands: which arguments are needed? How to set them?
+#         For example, put in localdefs the hand in day of the week? Make command line option?!
+#      8) Give the possibility to the user to create her/his own theme. Implement option
+#         to abilitate this and pass the file. This should contain the needed commands
+#         (\Heading, etc.) and it should be input in the main tex file. Add description
+#         to README file where commands to be provided should be listed.
