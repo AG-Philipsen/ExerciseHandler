AG-Philipsen/ExerciseHandler

commit 552afdf1d27cd63dc563ee3920edbed70ac919ca

Fix minor bug in command line option parser
When no primary option was given and still some secondary ones where
specified, the check on the scendary options was failing due to bash
syntax errors. This case was simply not considered, but it should give
to the user an understandable error. The situation is fixed now. Not
that I changed the way to get the last element of an array from
${ARRAY[-1]} to ${ARRAY: -1} since the first gives an error on empty
arrays, while the second just returns an empty string.

BashCode/CommandLineParser.bash
@@ -82,45 +82,45 @@ function ParseCommandLineParameters(){
                 EXHND_printVersion='TRUE'
                 shift ;;
             --exercises )
-                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed[-1]} $1
-                if [[ ${mutuallyExclusiveOptionsPassed[-1]} != '--makePresenceSheet'  &&  $2 =~ ^[1-9][0-9]*([,\-][1-9][0-9]*)*$ ]] ||
-                   [[ ${mutuallyExclusiveOptionsPassed[-1]}  = '--makePresenceSheet'  &&  $2 =~ ^[1-9][0-9]*([.][1-9][0-9]*)*([,][1-9][0-9]*([.][1-9][0-9]*)*)*$ ]]; then
+                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed: -1} $1
+                if [[ ${mutuallyExclusiveOptionsPassed: -1} != '--makePresenceSheet'  &&  $2 =~ ^[1-9][0-9]*([,\-][1-9][0-9]*)*$ ]] ||
+                   [[ ${mutuallyExclusiveOptionsPassed: -1}  = '--makePresenceSheet'  &&  $2 =~ ^[1-9][0-9]*([.][1-9][0-9]*)*([,][1-9][0-9]*([.][1-9][0-9]*)*)*$ ]]; then
                     EXHND_exercisesFromPoolAsNumbers="$2"
                 else
                     PrintError "The value of the option \"$1\" was not correctly specified!"; exit -1
                 fi
                 shift 2 ;;
             --exerciseSheetPostfix )
-                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed[-1]} $1
+                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed: -1} $1
                 EXHND_exerciseSheetSubtitlePostfix="$2"
                 shift 2 ;;
             --showAlsoSolutions )
-                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed[-1]} $1
+                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed: -1} $1
                 EXHND_showAlsoSolutions="TRUE"
                 shift ;;
             --ofExam )
-                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed[-1]} $1
+                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed: -1} $1
                 EXHND_solutionOfExam="TRUE"
                 shift ;;
             --sheetNumber )
-                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed[-1]} $1
+                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed: -1} $1
                 EXHND_sheetNumber="$2"
                 shift 2 ;;
             --final )
-                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed[-1]} $1
+                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed: -1} $1
                 EXHND_isFinal='TRUE'
                 shift ;;
             --fix )
-                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed[-1]} $1
+                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed: -1} $1
                 EXHND_isFinal='TRUE'
                 EXHND_fixFinal='TRUE'
                 shift ;;
             --showAllExercises )
-                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed[-1]} $1
+                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed: -1} $1
                 EXHND_displayAlreadyUsedExercises='TRUE'
                 shift ;;
             --themeFile )
-                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed[-1]} $1
+                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed: -1} $1
                 EXHND_userDefinedTheme="$2"
                 shift 2 ;;
             *)
@@ -276,6 +276,9 @@ function __static__GetKeyFromValueInArray(){
 }
 
 function __static__CheckSecondaryOption(){
+    if [ ${#mutuallyExclusiveOptionsPassed[@]} -eq 0 ]; then
+        PrintError "Secondary options were specified without any primary one!"; exit -1
+    fi
     local primaryOption secondaryOption
     primaryOption=$(__static__GetKeyFromValueInArray $1 mapOptions) || exit -1
     secondaryOption=$(__static__GetKeyFromValueInArray $2 mapOptions) || exit -1

commit dfb1db92a09512e63b4a00b5eed3f50a95396b78

Fix bug introduced in 552afdf
The correct access to the last element is ${ARRAY[@]: -1} and the [@]
part had been forgotten.

BashCode/CommandLineParser.bash
@@ -82,45 +82,45 @@ function ParseCommandLineParameters(){
                 EXHND_printVersion='TRUE'
                 shift ;;
             --exercises )
-                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed: -1} $1
-                if [[ ${mutuallyExclusiveOptionsPassed: -1} != '--makePresenceSheet'  &&  $2 =~ ^[1-9][0-9]*([,\-][1-9][0-9]*)*$ ]] ||
-                   [[ ${mutuallyExclusiveOptionsPassed: -1}  = '--makePresenceSheet'  &&  $2 =~ ^[1-9][0-9]*([.][1-9][0-9]*)*([,][1-9][0-9]*([.][1-9][0-9]*)*)*$ ]]; then
+                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed[@]: -1} $1
+                if [[ ${mutuallyExclusiveOptionsPassed[@]: -1} != '--makePresenceSheet'  &&  $2 =~ ^[1-9][0-9]*([,\-][1-9][0-9]*)*$ ]] ||
+                   [[ ${mutuallyExclusiveOptionsPassed[@]: -1}  = '--makePresenceSheet'  &&  $2 =~ ^[1-9][0-9]*([.][1-9][0-9]*)*([,][1-9][0-9]*([.][1-9][0-9]*)*)*$ ]]; then
                     EXHND_exercisesFromPoolAsNumbers="$2"
                 else
                     PrintError "The value of the option \"$1\" was not correctly specified!"; exit -1
                 fi
                 shift 2 ;;
             --exerciseSheetPostfix )
-                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed: -1} $1
+                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed[@]: -1} $1
                 EXHND_exerciseSheetSubtitlePostfix="$2"
                 shift 2 ;;
             --showAlsoSolutions )
-                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed: -1} $1
+                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed[@]: -1} $1
                 EXHND_showAlsoSolutions="TRUE"
                 shift ;;
             --ofExam )
-                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed: -1} $1
+                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed[@]: -1} $1
                 EXHND_solutionOfExam="TRUE"
                 shift ;;
             --sheetNumber )
-                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed: -1} $1
+                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed[@]: -1} $1
                 EXHND_sheetNumber="$2"
                 shift 2 ;;
             --final )
-                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed: -1} $1
+                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed[@]: -1} $1
                 EXHND_isFinal='TRUE'
                 shift ;;
             --fix )
-                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed: -1} $1
+                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed[@]: -1} $1
                 EXHND_isFinal='TRUE'
                 EXHND_fixFinal='TRUE'
                 shift ;;
             --showAllExercises )
-                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed: -1} $1
+                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed[@]: -1} $1
                 EXHND_displayAlreadyUsedExercises='TRUE'
                 shift ;;
             --themeFile )
-                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed: -1} $1
+                __static__CheckSecondaryOption ${mutuallyExclusiveOptionsPassed[@]: -1} $1
                 EXHND_userDefinedTheme="$2"
                 shift 2 ;;
             *)